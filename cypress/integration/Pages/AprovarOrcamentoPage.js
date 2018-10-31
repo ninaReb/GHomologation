2e7b3bfb7413a26325032d46de505c9ace52d0a7
Leonardo Goyanna <leonardo.goyanna@gmail.com>
2018-10-31 15:35:59 -0200

    Aprovar Orcamento Page [ci skip]

--- cypress/integration/Pages/AprovarOrcamentoPage.js
+++ cypress/integration/Pages/AprovarOrcamentoPage.js
@@ -1,12 +1,37 @@
-import Header from './Header.js';
-import OrcamentoRealizadoPage from './OrcamentoRealizadoPage.js';
-
 class AprovarOrcamentoPage {
-    constructor() {
-        this.header = new Header();
+
+    getPorcentagem(item) {
+        return cy.get('[test-id="orcamento-porcentagem-"' + item + ']');
+    }
+
+    getValor(item) {
+        return cy.get('[test-id="orcamento-valor-"' + item + ']');
+    }
+
+    getFaixa() {
+        return cy.get('[test-id="orcamento-faixa-supervisor"]');
+    }
+
+    getNotaVendedor() {
+        return cy.get('[test-id="orcamento-nota-vendedor"]');
+    }
+
+    getNota() {
+        return cy.get('[test-id="orcamento-nota-cliente"]');
     }
     
-    
+    aceitar() {
+        cy.get('[test-id="orcamento-aceitar"]').click();
+    }
+
+    rejeitar() {
+        cy.get('[test-id="orcamento-rejeitar"]').click();
+    }
+
+    cancelar() {
+        cy.get('[test-id="orcamento-rejeitar"]').click();
+    }
+
 }
-  
+
 export default AprovarOrcamentoPage;
